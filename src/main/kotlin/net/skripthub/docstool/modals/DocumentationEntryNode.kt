851980b1871167c47c8be6664af1df27ac1b9837
Fusezion <fusezionstream@gmail.com>
2025-03-07 02:54:43 -0500

    DocumentationEntryNode.kt - Add from method for class creation

--- src/main/kotlin/net/skripthub/docstool/modals/DocumentationEntryNode.kt
+++ src/main/kotlin/net/skripthub/docstool/modals/DocumentationEntryNode.kt
@@ -1,21 +1,25 @@
 package net.skripthub.docstool.modals
 
 import com.google.gson.JsonObject
+import org.skriptlang.skript.lang.entry.EntryData
+import org.skriptlang.skript.lang.entry.SectionEntryData
+import org.skriptlang.skript.lang.entry.util.TriggerEntryData
 
-data class DocumentationEntryNode (
-        val name: String,
-        val isRequired: Boolean,
-        val isSection: Boolean,
-        val defaultValue: String?
-) {
-    constructor(name: String,
-                isRequired: Boolean,
-                isSection: Boolean) : this(name, isRequired, isSection, null) {}
+data class DocumentationEntryNode(val name: String, val isRequired: Boolean, val isSection: Boolean, val defaultValue: String?) {
 
-    constructor(name: String,
-                isRequired: Boolean,
-                defaultValue: String) : this(name, isRequired, false, defaultValue) {}
-    fun getJsonElement(): JsonObject {
+    companion object {
+
+        fun from(entry: EntryData<*>) : DocumentationEntryNode {
+            return DocumentationEntryNode(
+                entry.key,
+                !entry.isOptional,
+                entry is SectionEntryData || entry is TriggerEntryData,
+                entry.defaultValue?.toString()
+            )
+        }
+    }
+
+    fun asJsonElement(): JsonObject {
         val jsonNode = JsonObject()
         jsonNode.addProperty("name", name)
         jsonNode.addProperty("isRequired", isRequired)
