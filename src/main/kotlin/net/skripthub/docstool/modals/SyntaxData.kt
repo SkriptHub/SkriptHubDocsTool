26613336fce8fd87838d659c77c42aa96bc4e8b7
Fusezion <fusezionstream@gmail.com>
2025-03-07 04:03:33 -0500

    SyntaxData.kt - Mild Changes - Add keywords support - Changed to different null handling

--- src/main/kotlin/net/skripthub/docstool/modals/SyntaxData.kt
+++ src/main/kotlin/net/skripthub/docstool/modals/SyntaxData.kt
@@ -1,5 +1,8 @@
 package net.skripthub.docstool.modals
 
+import ch.njol.util.StringUtils
+
+@Suppress("ArrayInDataClass")
 data class SyntaxData(var id: String? = null,
                       var name: String? = null,
                       var since: Array<String>? = null,
@@ -12,70 +15,36 @@ data class SyntaxData(var id: String? = null,
                       var changers: Array<String>? = null,
                       var eventValues: Array<String>? = null,
                       var cancellable: Boolean? = null,
+                      var keywords: Array<String>? = null,
                       var entries: Array<DocumentationEntryNode>? = null,
-                      ) {
+    ) {
+
     fun toMap(): Map<String, Any> {
         val map = LinkedHashMap<String, Any>()
-        addProperty(map, "ID", id!!)
-        addProperty(map, "Name", name!!)
-        if(description != null){
-            addArray(map, "Description", description!!)
-        }
-        if (examples != null){
-            addArray(map, "Examples", examples!!)
-        }
-        if(usage != null){
-            addProperty(map, "Usage", *usage!!)
-        }
-        if(since != null){
-            addArray(map, "Since", since!!)
-        }
-        if(returnType != null){
-            addProperty(map, "Return type", returnType!!)
-        }
-        if(changers != null){
-            addArray(map, "Changers", changers!!)
-        }
-        addArray(map, "Patterns", patterns!!)
-        if(eventValues != null){
-            addArray(map, "Event values", eventValues!!)
-        }
-        if (cancellable != null) {
-            map["Cancellable"] = cancellable!!
-        }
-        if (requiredPlugins != null){
-            addArray(map, "Required Plugins", requiredPlugins!!)
-        }
-        if (entries != null){
-            addArray(map, "Entries", entries!!)
-        }
+
+        addProperty(map, "id", id!!)
+        addProperty(map, "name", name!!)
+
+        addProperty(map, "return_type", returnType)
+        addArray(map, "description", description)
+        addArray(map, "examples", examples)
+        addArray(map, "since", since)
+        addArray(map, "patterns", patterns)
+        addArray(map, "Changers", changers)
+        addArray(map, "event_values", eventValues)
+        addArray(map, "required_plugins", requiredPlugins)
+        addArray(map, "entries", entries)
+        addArray(map, "keywords", keywords)
+        usage?.let { addProperty(map, "usage", *it) }
+        cancellable?.let { map["cancellable"] = it }
         return map
     }
 
-    private fun addArray(map: LinkedHashMap<String, Any>, property: String, array: Array<DocumentationEntryNode>) {
-        if (array.isEmpty())
-            return
-        map[property] = array
+    private fun addProperty(map: MutableMap<String, Any>, property: String, vararg values: String?) {
+        map[property] = if (values.filterNotNull().isNotEmpty()) StringUtils.join(values.filterNotNull(), "\n") else return
     }
 
-    private fun addProperty(map: MutableMap<String, Any>, property: String, vararg value: String) {
-        if (value.isEmpty())
-            return
-        val sb = StringBuilder()
-        for (str in value) {
-            if (str.isNotEmpty()) {
-                if (sb.isNotEmpty())
-                    sb.append("\n")
-                sb.append(str)
-            }
-        }
-        if (sb.isNotEmpty())
-            map[property] = sb.toString()
-    }
-
-    private fun addArray(map: MutableMap<String, Any>, property: String, array: Array<String>) {
-        if (array.isEmpty())
-            return
-        map[property] = array
+    private fun <T> addArray(map: MutableMap<String, Any>, property: String, array: Array<T>?) {
+        map[property] = if (!array.isNullOrEmpty()) array else return
     }
 }
