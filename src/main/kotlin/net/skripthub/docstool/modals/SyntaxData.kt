21a863ea754c290d88f707d731d34a5fb4f4ac7f
Fusezion <fusezionstream@gmail.com>
2025-03-08 00:25:16 -0500

    SyntaxData.kt - fix JSON mapping and limit file changes

--- src/main/kotlin/net/skripthub/docstool/modals/SyntaxData.kt
+++ src/main/kotlin/net/skripthub/docstool/modals/SyntaxData.kt
@@ -6,15 +6,15 @@ import ch.njol.util.StringUtils
 data class SyntaxData(var id: String? = null,
                       var name: String? = null,
                       var since: Array<String>? = null,
-                      var returnType: String? = null,
                       var requiredPlugins: Array<String>? = null,
                       var description: Array<String>? = null,
                       var examples: Array<String>? = null,
+                      var returnType: String? = null,
+                      var cancellable: Boolean? = null,
                       var patterns: Array<String>? = null,
                       var usage: Array<String>? = null,
                       var changers: Array<String>? = null,
                       var eventValues: Array<String>? = null,
-                      var cancellable: Boolean? = null,
                       var keywords: Array<String>? = null,
                       var entries: Array<DocumentationEntryNode>? = null,
     ) {
@@ -25,14 +25,14 @@ data class SyntaxData(var id: String? = null,
         addProperty(map, "id", id!!)
         addProperty(map, "name", name!!)
 
-        addProperty(map, "return_type", returnType)
+        addProperty(map, "return type", returnType)
         addArray(map, "description", description)
         addArray(map, "examples", examples)
         addArray(map, "since", since)
         addArray(map, "patterns", patterns)
         addArray(map, "changers", changers)
-        addArray(map, "event_values", eventValues)
-        addArray(map, "required_plugins", requiredPlugins)
+        addArray(map, "event values", eventValues)
+        addArray(map, "required plugins", requiredPlugins)
         addArray(map, "entries", entries)
         addArray(map, "keywords", keywords)
         usage?.let { addProperty(map, "usage", *it) }
