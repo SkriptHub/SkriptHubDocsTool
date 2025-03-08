6cbddd52c4c4e57d8e0c743925f9b50a0880d08d
Fusezion <fusezionstream@gmail.com>
2025-03-08 02:00:36 -0500

    SyntaxData.kt - Fix placement to limit file changes

--- src/main/kotlin/net/skripthub/docstool/modals/SyntaxData.kt
+++ src/main/kotlin/net/skripthub/docstool/modals/SyntaxData.kt
@@ -5,18 +5,18 @@ import ch.njol.util.StringUtils
 @Suppress("ArrayInDataClass")
 data class SyntaxData(var id: String? = null,
                       var name: String? = null,
-                      var since: Array<String>? = null,
-                      var requiredPlugins: Array<String>? = null,
                       var description: Array<String>? = null,
                       var examples: Array<String>? = null,
-                      var returnType: String? = null,
-                      var cancellable: Boolean? = null,
-                      var patterns: Array<String>? = null,
                       var usage: Array<String>? = null,
+                      var since: Array<String>? = null,
+                      var returnType: String? = null,
                       var changers: Array<String>? = null,
+                      var patterns: Array<String>? = null,
                       var eventValues: Array<String>? = null,
-                      var keywords: Array<String>? = null,
+                      var cancellable: Boolean? = null,
+                      var requiredPlugins: Array<String>? = null,
                       var entries: Array<DocumentationEntryNode>? = null,
+                      var keywords: Array<String>? = null,
     ) {
 
     fun toMap(): Map<String, Any> {
@@ -24,19 +24,18 @@ data class SyntaxData(var id: String? = null,
 
         addProperty(map, "id", id!!)
         addProperty(map, "name", name!!)
-
-        addProperty(map, "return type", returnType)
         addArray(map, "description", description)
         addArray(map, "examples", examples)
+        usage?.let { addProperty(map, "usage", *it) }
         addArray(map, "since", since)
-        addArray(map, "patterns", patterns)
+        addProperty(map, "return type", returnType)
         addArray(map, "changers", changers)
+        addArray(map, "patterns", patterns)
         addArray(map, "event values", eventValues)
+        cancellable?.let { map["cancellable"] = it }
         addArray(map, "required plugins", requiredPlugins)
         addArray(map, "entries", entries)
         addArray(map, "keywords", keywords)
-        usage?.let { addProperty(map, "usage", *it) }
-        cancellable?.let { map["cancellable"] = it }
         return map
     }
 
