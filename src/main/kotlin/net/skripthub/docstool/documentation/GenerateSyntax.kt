74665e3f17d097644d07abc870742f079528b7b1
Fusezion <fusezionstream@gmail.com>
2025-04-06 01:30:49 -0400

    Remove debug import

--- src/main/kotlin/net/skripthub/docstool/documentation/GenerateSyntax.kt
+++ src/main/kotlin/net/skripthub/docstool/documentation/GenerateSyntax.kt
@@ -1,6 +1,5 @@
 package net.skripthub.docstool.documentation
 
-import ch.njol.skript.Skript
 import ch.njol.skript.classes.Changer.ChangeMode
 import ch.njol.skript.classes.ClassInfo
 import ch.njol.skript.doc.*
